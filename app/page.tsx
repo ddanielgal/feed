11113d2fddd70c9d8271b722cc4c8006b1796dbf
ddanielgal <denxyzw@gmail.com>
2023-07-17 21:25:56 +0200

    fetch from the pragmatic eng

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import { use } from "react";
+import Parser from "rss-parser";
+
 export default function Home() {
-  return <main>Hello Feed</main>;
+  const parser = new Parser();
+  const feed = use(
+    parser.parseURL("https://newsletter.pragmaticengineer.com/feed")
+  );
+
+  return (
+    <main>
+      <h1>{feed.title}</h1>
+      <ul>
+        {feed.items.map((item) => (
+          <li key={item.guid}>
+            <a href={item.link}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
