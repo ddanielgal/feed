a887ed2d55db932174f39c0256548ad173fc7bb0
ddanielgal <denxyzw@gmail.com>
2023-07-19 22:39:17 +0200

    add new feed action

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { UserButton } from "@clerk/nextjs";
 import { use } from "react";
 import Parser from "rss-parser";
+import { z } from "zod";
+import { prisma } from "./db";
 
 export default function Home() {
   const parser = new Parser();
@@ -8,9 +10,19 @@ export default function Home() {
     parser.parseURL("https://newsletter.pragmaticengineer.com/feed")
   );
 
+  async function addFeed(data: FormData) {
+    "use server";
+    const url = z.string().url().parse(data.get("url"));
+    await prisma.feed.create({ data: { url } });
+  }
+
   return (
     <main>
       <UserButton afterSignOutUrl="/" />
+      <form action={addFeed}>
+        <input type="text" name="url" />
+        <button type="submit">Add Feed</button>
+      </form>
       <h1>{feed.title}</h1>
       <ul>
         {feed.items.map((item) => (
